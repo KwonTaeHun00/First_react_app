5029d9d46922375f0e4bd6a335b1ba613b1baa30
Tae.k2 <56614716+KwonTaeHun00@users.noreply.github.com>
2022-05-11 17:18:26 +0900

    Movie App
    I'm going to decorate more later.

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
-import {useState, useEffect} from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run only once");
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(()=>{
-    console.log("I run when 'counter' changes.")
-  }, [counter]);
-  useEffect(()=>{
-    console.log("I run when keyword & counter change");
-  }, [keyword, counter]);
   return (
-    <div>
-      <input
-       value = {keyword} 
-       onChange={onChange} 
-       type ="text" 
-       placeholder="Search here" />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />}/>
+        <Route path="/movie/:id" element={<Detail />}/>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
   );
 }
 
